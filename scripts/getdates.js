62d33b8a48beac3b35c757480512e22e3aaedfe0
Diliana007 <dilianathompson@gmail.com>
2025-07-15 13:03:19 +0100

    Finished

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,5 @@
-// Dynamically set the current year
-document.getElementById("currentyear").textContent = new Date().getFullYear();
+// Set current year in footer
+document.getElementById('currentyear').textContent = new Date().getFullYear();
 
-// Dynamically set the last modified date
-document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
+// Set last modified date
+document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
